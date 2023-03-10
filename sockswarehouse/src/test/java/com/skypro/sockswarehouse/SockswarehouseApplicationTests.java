3f0634e046f0af1d7460e52ed4db825df7812624
Dmitry <kirillovdmitrii_at@mail.ru>
2023-03-10 11:51:30 +0500

    init

---
+++ sockswarehouse/src/test/java/com/skypro/sockswarehouse/SockswarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.sockswarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockswarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
